387c4e612b320673a302ed431bc7173cc97e7e95
Hector Anton <hectoranf@gmail.com>
2021-07-08 21:45:47 +0200

    add database configuration

--- server/app.js
+++ server/app.js
@@ -1,7 +1,12 @@
 require('dotenv').config()
 
+// Database config
+require('./config/mongoose.config')
+
 // App
 const express = require('express')
 const app = express()
+
+require('./config/middleware.config')(app)
 
 module.exports = app
---
+++ server/config/mongoose.config.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+mongoose
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(details => console.log(`Connected to Mongo! Database name: "${details.connections[0].name}"`))
+    .catch(err => console.error('Error connecting to mongo', err))
+
+module.exports = mongoose
