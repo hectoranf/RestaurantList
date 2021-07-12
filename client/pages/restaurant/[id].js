eeae278f90a1002c06bc32d39ecb8d294eac62d1
Hector Anton <hectoranf@gmail.com>
2021-07-12 15:30:18 +0200

    server side propr

--- client/pages/restaurant/[id].js
+++ client/pages/restaurant/[id].js
@@ -90,7 +90,7 @@ export async function getStaticPaths() {
   }
 }
 
-export async function getStaticProps({ params }) {
+export async function getServerSideProps({ params }) {
   return await getOneRestaurant(params.id).then((res) => {
     return {
       props: {
