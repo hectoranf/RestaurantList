5f63f3c2e55a3a97138cadbab40258e1405ca3b6
Hector Anton <hectoranf@gmail.com>
2021-07-16 12:48:50 +0200

    fix user routes

--- server/bin/seed.js
+++ server/bin/seed.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose")
-const Restaurant = require("./../models/restaurant.model")
-const restaurantList = require("./restaurants.json")
+const mongoose = require('mongoose')
+const Restaurant = require('./../models/restaurant.model')
+const restaurantList = require('./restaurants.json')
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 })
 
 Restaurant.collection
-  .drop()
-  .then(() => Restaurant.create(restaurantList))
-  .then((res) => {
-    console.log(`${res.length} restaurants were created.`)
-    mongoose.connection.close()
-  })
-  .catch((err) => console.log(`An ERROR occurred: ${err}`))
+	.drop()
+	.then(() => Restaurant.create(restaurantList))
+	.then((res) => {
+		console.log(`${res.length} restaurants were created.`)
+		mongoose.connection.close()
+	})
+	.catch((err) => console.log(`An ERROR occurred: ${err}`))
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,16 +2,22 @@ const router = require('express').Router()
 const User = require('../models/user.model')
 
 router.get('/', (req, res, next) => {
-	const { _id, username, favouriteRestaurants, role } = req.user
-	res.json({
-		msg: 'You made it to the secure route',
-		user: { _id, username, favouriteRestaurants, role },
-	})
+	User.findById(req.user._id)
+		.populate('favouriteRestaurants')
+		.then((response) => {
+			const { _id, username, favouriteRestaurants } = response
+			console.log(_id)
+			return res.json({
+				user: { _id, username, favouriteRestaurants },
+			})
+		})
+		.catch((err) => next(err))
 })
 
 //Update favourites/unfavourites
 router.put('/updateFavourites/:_id', (req, res) => {
 	User.findByIdAndUpdate(req.params._id, { favouriteRestaurants: req.body }, { new: true })
+		.populate('favouriteRestaurants')
 		.then((response) => res.json(response))
 		.catch((err) => res.status(500).json(err))
 })
